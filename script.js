0f09f4163ab5cb186d6e95b8f1111b323c1bb452
Tabish <tabishbashir@gmail.com>
2018-03-20 20:34:26 +0530

    Bug fixes for input function

--- script.js
+++ script.js
@@ -1,40 +1,82 @@
 let current_val = 0;
+let last_operation;  let start = '';
+let prev_num ='' ; let curr_num ='';
 
-function operate(operator = '+' , val = 0){
-    operations[operator](val);
-    console.log('current_val =',current_val);
+function operate(p_num, c_num, operator){
+    operations[operator](p_num,c_num);
 }
 
+function clear(){
+
+      let clear = document.querySelector('.clear');
+      let display = document.querySelector('.screen');
+
+      clear.onclick = function(){
+          current_val = 0;
+          last_operation;  start = '';
+          prev_num ='' ; curr_num ='';
+          display.innerHTML = current_val;
+      }
+}
 function input(){
 
       let buttons = document.querySelectorAll('.button');
-      let number = '';
-      let last_operation;
+      let display = document.querySelector('.screen');
+
 
       buttons.forEach(function(button){
             button.onclick = function(e){
                 let temp = e.target.innerHTML;
-                console.log(temp);
-                if(temp in operations || temp == '='){
-                    if (temp != '='){
-                      operations[temp](+number);
+                display.innerHTML = temp;
+
+                if( temp in operations || temp == '=' ){
+                    if (temp != '=') {
+                        if(last_operation){
+                          operations[last_operation](+curr_num,+prev_num)
+                          prev_num = current_val;
+                          curr_num = '';
+                          last_operation = temp;
+                          display.innerHTML = current_val;
+                        }
+                        else {
+                          last_operation = temp;
+                          start= 'start';
+                        }
                     }
-                    else{
-                      operate(operator =last_operation,val = +number);
+                    else {
+
+                        if(!last_operation) {             // use of '=' without prev_num or last_operation
+                            display.innerHTML = current_val;                            // initialization
+                          }
+                        else{
+                            operate(+prev_num,+curr_num,last_operation)
+                            prev_num = current_val;
+                            curr_num = '';
+                            last_operation = '';
+                            display.innerHTML = current_val;
+                         }
                     }
-                    number = '';   last_operation = temp;
-              //      console.log('current_val =',current_val);
                 }
-                else{number += temp;}
-                console.log(number);
-            }
-      })
+                else {
+                    if (start) {
+                      curr_num+= temp;
+                      display.innerHTML = curr_num;
+
+                    }
+                    else {
+                      prev_num+=temp;
+                      display.innerHTML = prev_num;
+                    }
+                }
+
+      }})
 }
 
 input();
+clear();
 
-let operations = {"+": (a)=> ( current_val+=a),
-                  "-": (a)=> ( current_val-=a),
-                  "*": function(a){if(current_val == 0) current_val = 1; current_val*=a;} ,
-                  "/": function(a){if(a == 0) return; current_val/=a;}
+let operations = {"+": (a,b)=> ( current_val =a+b),
+                  "-": (a,b)=> ( current_val =a-b),
+                  "*": function(a,b){if(current_val == 0){ current_val = 1;}; current_val =a*b;} ,
+                  "/": function(a,b){if(a == 0) return; current_val=a/b;}
                   }
